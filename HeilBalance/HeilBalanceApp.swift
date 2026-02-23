5679d30b84bfe3f26f2bf6f48af36187fcbbed2e
HugoHartmann777 <hugo_hartmann777@icloud.com>
2026-02-23 20:12:52 +0100

    Initial Commit

---
+++ HeilBalance/HeilBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeilBalanceApp.swift
+//  HeilBalance
+//
+//  Created by Hugo on 23.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HeilBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
